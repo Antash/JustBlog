0f9d7d05157d5d8e0a0289b6cead5f83b05d2348
Anton Ashmarin <aashmarin@gmail.com>
2017-06-24 17:48:14 +0300

    post editor wip

--- JustBlog/Scripts/App/PostEditor/postEditor.tsx
+++ JustBlog/Scripts/App/PostEditor/postEditor.tsx
@@ -1,20 +1,42 @@
-﻿/*import React from 'react';
-import { Editor, EditorState } from 'draft-js';
+﻿import React from 'react';
+import { Editor, EditorState, RichUtils } from 'draft-js';
+import EditorToolbar from './editorToolbar';
 
 export interface IMyEditorState {
-    state: EditorState,
-    onChange: (state: EditorState) => void
+    editorState: EditorState;
 }
 
-export default class MyEditor extends React.Component<IMyEditorState, IMyEditorState> {
-    constructor(props: IMyEditorState) {
+export default class PostEditor extends React.Component<{}, IMyEditorState> {
+    constructor(props: any) {
         super(props);
-        this.state = { editorState: EditorState.createEmpty(),  };
-        this.state.onChange = (editorState) => this.setState({ editorState });
+        this.state = { editorState: EditorState.createEmpty() };
+    }
+    onChange(e: EditorState) {
+        this.setState({ editorState: e });
+    }
+    handleKeyCommand(command: any) {
+        const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+        if (newState) {
+            this.onChange(newState);
+            return 'handled';
+        }
+        return 'not-handled';
+    }
+    handleStyleChanged(style: any) {
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, style));
     }
     render() {
         return (
-            <Editor editorState={this.state.editorState} onChange={this.onChange} />
+            <div>
+                <EditorToolbar onStyleChanged={this.handleStyleChanged.bind(this)} />
+                <div className="editor">
+                    <Editor
+                        editorState={this.state.editorState}
+                        handleKeyCommand={this.handleKeyCommand.bind(this)}
+                        onChange={this.onChange.bind(this)}
+                    />
+                </div>
+            </div>
         );
     }
-}*/
+}
--- JustBlog/Scripts/App/index.tsx
+++ JustBlog/Scripts/App/index.tsx
@@ -1,8 +1,14 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentBox from './CommentBox/commentBox';
+import PostEditor from './PostEditor/postEditor';
 
 ReactDOM.render(
     <CommentBox url="/comments" submitUrl="/comments/new" pollInterval={2000} />,
-    document.getElementById("container")
+    document.getElementById("commentBox")
+);
+
+ReactDOM.render(
+    <PostEditor />,
+    document.getElementById("postEditor")
 );
